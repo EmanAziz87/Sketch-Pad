811efef4ffa1c05efaf5714e783904a4ea617170
Eman Aziz <aziz.eman@hotmail.com>
2022-01-20 15:43:44 -0500

    fixed aspect ratio of squares in grid. created prompt that adjusts grid size. refactored code into functions

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 "use strict"
 
-// Creates 16 Squares and gives them the same class
-for (let i = 1; i <= 256; i++) {
-    const container = document.querySelector('.container');
-    const div = document.createElement('div');
-    div.classList.add('square', `square${i}`);
-    container.appendChild(div);
+const container = document.querySelector('.container');
+
+function createGrid(size) {
+    for (let i = 1; i <= size ** 2; i++) {
+        const div = document.createElement('div');
+        div.classList.add('square', `square${i}`);
+        container.appendChild(div);
+        container.style.removeProperty('grid-template-column');
+        container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    }
+    colorSquares();
 }
 
-const allSquare = document.querySelectorAll('.square');
+function chooseSize() {
+    let size = (Number(prompt("Pick Your Sketchpad Size! (Tip: Must be 64 or less)")));
+    if (size > 64 || isNaN(size) || !size) {
+        alert("The number must be below 64");
+        return createGrid(16);
+    }
+    return createGrid(size);
+}
 
-allSquare.forEach((square) => {
-    square.addEventListener('mouseover', function() {
-        square.style.cssText = 'background-color: black';
+function colorSquares() {
+    const allSquare = document.querySelectorAll('.square');
+    allSquare.forEach((square) => {
+        square.addEventListener('mouseover', function() {
+            square.style.cssText = 'background-color: black';
     });
 });
-   
+}
+
     
+chooseSize();
 
 
 
